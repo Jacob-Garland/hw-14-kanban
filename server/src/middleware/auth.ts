70970531b018a1db82611864e9db9de8c89e487d
Jacob Garland <352cjg@gmail.com>
2025-01-19 13:13:34 -0500

    debugged, login issues

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,17 +7,22 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) {
-    return res.sendStatus(401);
-  } else {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
       if (err) {
-        return res.sendStatus(403);
+        return res.sendStatus(403); // Forbidden
       }
+
       req.user = user as JwtPayload;
-      next();
+      return next();
     });
+  } else {
+    res.sendStatus(401); // Unauthorized
   }
 };
